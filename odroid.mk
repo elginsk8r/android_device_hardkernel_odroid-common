1a5be1e91efe3ce1d10563d0d2a5841dd8523676
Nick Reuter <nreuter85@gmail.com>
2022-07-09 22:06:36 -0500

    odroid-common: update partitions
    Change-Id: I402f6ef96d0c3d4f2da998d85950a3022ca62fda

--- odroid.mk
+++ odroid.mk
@@ -95,9 +95,6 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1-service
 endif
 
-## Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 ## Permissions
 PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.device_unique_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.device_unique_attestation.xml \
--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -27,23 +27,16 @@ def AddDtbImage(info, folder, basename):
   data = info.input_zip.read(folder + basename)
   common.ZipWriteStr(info.output_zip, name, data)
   info.script.AppendExtra('package_extract_file("%s", "/tmp/dtb.img");' % name);
-  info.script.AppendExtra('run_program("/system/bin/dd", "if=/tmp/dtb.img", "of=/dev/dtb", "bs=1k", "count=256");');
+  info.script.AppendExtra('run_program("/system/bin/dd", "if=/tmp/dtb.img", "of=/dev/block/dtbs", "bs=1k", "count=256");');
 
 def PrintInfo(info, dest):
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
 
 def OTA_InstallEnd(info):
-  PrintInfo(info, "/dev/block/by-name/dtbo")
-  AddImage(info, "IMAGES/", "dtbo.img", "/dev/block/by-name/dtbo")
-  PrintInfo(info, "/dev/block/by-name/vbmeta")
-  AddImage(info, "IMAGES/", "vbmeta.img", "/dev/block/by-name/vbmeta")
   if 'RADIO/dtb.img' in info.input_zip.namelist():
-    PrintInfo(info, "/dev/dtb")
+    PrintInfo(info, "/dev/block/dtbs")
     AddDtbImage(info, "RADIO/", "dtb.img")
-  if 'RADIO/logo.img' in info.input_zip.namelist():
-    PrintInfo(info, "/dev/block/by-name/logo")
-    AddImage(info, "RADIO/", "logo.img", "/dev/block/by-name/logo")
   if 'RADIO/bootloader.img' in info.input_zip.namelist():
-    PrintInfo(info, "/dev/block/by-name/bootloader")
-    AddImage(info, "RADIO/", "bootloader.img", "/dev/block/by-name/bootloader")
+    PrintInfo(info, "/dev/block/bootloader")
+    AddImage(info, "RADIO/", "bootloader.img", "/dev/block/bootloader")
   return
